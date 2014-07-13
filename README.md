1e36f330e05e94b3a40793ec26c29fe955e75b42
jeaye <contact@jeaye.com>
2014-07-13 23:18:30 +0800

    Added initial tumblr workings

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 fu-er.com
 =========
+
+Source for http://fu-er.com. This website is run as a Ruby web application using Sinatra.
+
+Required gems:
+  - sinatra
+  - tumblr_client
--- app.rb
+++ app.rb
@@ -1,9 +1,28 @@
 require 'sinatra'
+require 'tumblr_client'
 
 set :public_folder, File.dirname(__FILE__)
 
+# Stored in a file outside of this repo
+cfg = File.new('oauth_config').readlines.collect{ |l| l.chomp }
+Tumblr.configure do |config|
+  config.consumer_key = cfg[0]
+  config.consumer_secret = cfg[1]
+  config.oauth_token = cfg[2]
+  config.oauth_token_secret = cfg[3]
+end
+
 get '/' do
-  File.new("index.html").readlines
+  #File.new("index.html").readlines
+  
+  data = ""
+  
+  tumblr = Tumblr::Client.new
+  posts = tumblr.posts('fu-er.tumblr.com')['posts']
+  posts.each { |p|
+    data += '<hr/>' + p['body']
+  }
+  data
 end
 
 get '/resume' do
