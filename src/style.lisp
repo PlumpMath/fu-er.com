7a2f5a47402cb4b774f04c20456d67c7ad740663
jeaye <contact@jeaye.com>
2015-10-01 15:27:32 +0800

    Add in a better background color

--- src/style.lisp
+++ src/style.lisp
@@ -3,63 +3,61 @@
 (in-package :jank-repl.style)
 
 (define-easy-handler (main-css :uri "/main.css") ()
-  (setf (content-type*) "text/css")
-  (css
-   '(("html"
-      :height "100%")
-     
-     ("body"
-      :margin "0"
-      :padding "0"
-      :height "100%")
+  (let ((global-background-color '(:background-color "#c9c4b3")))
+    (setf (content-type*) "text/css")
+    (css
+    `(("html"
+        :height "100%")
 
-     (".content"
-      :margin-left "200px"
-      :height "100%"
-      :width "auto"
-      :position "relative"
-      :overflow "auto"
-      :background-color "blue")
+        ("body"
+        :margin "0"
+        :padding "0"
+        :height "100%")
 
-     (".sidebar"
-      :position "fixed"
-      :min-width "200px"
-      :height "100%"
-      :overflow "hidden"
-      :background-color "red")
+        (".content"
+        :margin-left "200px"
+        :height "100%"
+        :width "auto"
+        :position "relative"
+        :overflow "auto"
+        ,@global-background-color)
 
-     (".sidebar-banner"
-      :position "absolute"
-      :top "0px"
-      :left "0px"
-      :min-width "200px"
-      :height "200px"
-      :background-color "grey")
+        (".sidebar"
+        :position "fixed"
+        :min-width "200px"
+        :height "100%"
+        :overflow "hidden"
+        ,@global-background-color)
 
-     (".sidebar-banner-image"
-      ; Centered
-      :position "absolute"
-      :top "25px"
-      :left "25px"
+        (".sidebar-banner"
+        :position "absolute"
+        :top "0px"
+        :left "0px"
+        :min-width "200px"
+        :height "200px")
 
-      :width "150px"
-      :height "150px")
+        (".sidebar-banner-image"
+        ; Centered
+        :position "absolute"
+        :top "25px"
+        :left "25px"
 
-     (".sidebar-content"
-      :position "absolute"
-      :top "200px"
-      :left "0px"
-      :bottom "0px"
-      :overflow "auto"
-      :min-width "200px"
-      :background-color "green"
+        :width "150px"
+        :height "150px")
 
-      ; Move the scroll bar out of sight
-      :max-height "100%"
-      :margin-right "-100px"
-      :padding-right "100px")
+        (".sidebar-content"
+        :position "absolute"
+        :top "200px"
+        :left "0px"
+        :bottom "0px"
+        :overflow "auto"
+        :min-width "200px"
 
-     (".sidebar-nav"
-       :list-style-type "none")
+        ; Move the scroll bar out of sight
+        :max-height "100%"
+        :margin-right "-100px"
+        :padding-right "100px")
 
-     )))
+        (".sidebar-nav"
+        :list-style-type "none")
+        ))))
