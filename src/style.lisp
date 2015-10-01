d95620603c90a377d16096b925b611768c3be467
jeaye <contact@jeaye.com>
2015-10-01 16:11:14 +0800

    Correct some sidebar image positioning

--- src/style.lisp
+++ src/style.lisp
@@ -3,6 +3,7 @@
 (in-package :jank-repl.style)
 
 (define-easy-handler (main-css :uri "/main.css") ()
+  ; TODO: Pull out 250 and 227
   (let ((global-background-color '(:background-color "#c9c4b3")))
     (setf (content-type*) "text/css")
     (css
@@ -15,7 +16,7 @@
         :height "100%")
 
         (".content"
-        :margin-left "200px"
+        :margin-left "250px"
         :height "100%"
         :width "auto"
         :position "relative"
@@ -24,7 +25,7 @@
 
         (".sidebar"
         :position "fixed"
-        :min-width "200px"
+        :min-width "250px"
         :height "100%"
         :overflow "hidden"
         ,@global-background-color)
@@ -33,33 +34,32 @@
         :position "absolute"
         :top "0px"
         :left "0px"
-        :min-width "200px"
-        :height "200px"
+        :min-width "250px"
+        :height "227px"
         :background-image "url(\"/sidebar_top.png\")")
 
         (".sidebar-banner-image"
         ; Centered
+        ; TODO: fudged
         :position "absolute"
         :top "25px"
-        :left "25px"
-
-        :width "150px"
-        :height "150px")
+        :left "10px")
 
         (".sidebar-content"
         :position "absolute"
-        :top "200px"
+        :top "227px"
         :left "0px"
         :bottom "0px"
         :overflow "auto"
-        :min-width "200px"
+        :min-width "250px"
 
         ; Move the scroll bar out of sight
         :max-height "100%"
         :margin-right "-100px"
         :padding-right "100px"
 
-        :background-image "url(\"/sidebar_body.png\")")
+        :background-image "url(\"/sidebar_body.png\")"
+        :background-repeat "repeat-y")
 
         (".sidebar-nav"
         :list-style-type "none")
